6d748a1f50cc322bd932aedc3903629be902fd05
19008634 <19008634@DESKTOP-7P7DAQM>
2020-08-23 19:46:49 +0800

    finished menu ry

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,91 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+	
+	private static ArrayList<Reigstration> registrationList;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int option = 0;
+		while (option != 6) {
+			
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				// Member 1 pls fill up
+				
+
+			} else if (option == 2) {
+				// Member 2 pls fill up
+				
+
+			} else if (option == 3) {
+				// Member 3 pls fill up
+				
+
+			} else if (option == 4) {
+				// Member 4 pls fill up
+				
+
+			
+			
+			
+			} else if (option == 5) {
+				// Member 5
+			int registration_option = Helper.readInt("Enter an option > ");
+			//sub menu for registration
+			if(registration_option == 1) {
+				//
+				int registration_number = Helper.readInt("Enter registration number > ");
+				int course_schedule_id =  Helper.readInt("Enter course schdeule id > ");
+				
+				String registration_date = Helper.readString("Enter date in this format (DD/MM/YYYY > ");
+				String registration_time = Helper.readString("Enter time in 24hr format");
+				
+				if(memberArrayList.contains(member)) {
+					registrationArrayList.add(new Registration(member, registration_date, registration_time));
+				}
+			}else if(registration_option == 2) {
+				// view all registrations
+				
+			}else if (registration_option == 3) {
+				//delete registration
+				
+			}
+			
+			
+			
+			
+			} else if (option == 6) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
+	}
+	
+	
+	//menu for course managment system
+	public static void menu() {
+		C206_CaseStudy.setHeader("Course Management System");
+		System.out.println("1. Members");
+		System.out.println("2. Course category");
+		System.out.println("3. Course");
+		System.out.println("4. Course schedule");
+		System.out.println("5. Registration");
+		System.out.println("6. Quit");
+		Helper.line(80, "-");
+
+
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
 	}
 
 }
---
+++ C206_CaseStudy/src/Registration.java
@@ -0,0 +1,16 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19008634, 23 Aug 2020 7:35:49 pm
+ */
+
+public class Registration {
+ 
+	public int registration_number;
+	public int course_schedule_id;
+	public String email;
+	
+}
