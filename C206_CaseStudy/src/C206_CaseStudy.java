ec217b09f8320cc3770a9b9bfa54b72bb3683453
zolynn07 <68525185+zolynn07@users.noreply.github.com>
2020-08-24 14:12:45 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -42,9 +42,25 @@ public class C206_CaseStudy {
 					}
 					}
 				}
-				else{
-				
+				else if (registerMember_option == 3) {
+					String email = Helper.readString("Enter email > ");
+					for(int i = 0; i < registrationArrayList.size(); i++) {
+						if(memberArrayList.isEmpty()==false) {
+							if(memberArrayList.get(i).getEmail() == email){
+								System.out.println("Member deleted");
+							}
+							else {
+								System.out.println("Enter valid email");
+							}
+						}
 				}
+				}	
+				else{
+				System.out.println("Please enter a correct option, Thank you");
+				}
+				
+				
+				
 				
 				
 				
