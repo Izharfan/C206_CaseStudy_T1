dd3330605ec9fd0dd49c03c965bef1bd33c273d8
zolynn07 <68525185+zolynn07@users.noreply.github.com>
2020-08-29 23:36:29 +0800

    Update MemberDB.java

--- C206_CaseStudy/src/MemberDB.java
+++ C206_CaseStudy/src/MemberDB.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /**
  * I declare that this code was written by me.
  * I will not copy or allow others to copy my code.
@@ -55,6 +53,93 @@ public class MemberDB {
 		return output;
 	}
 	
+	public static void deleteMember (String email) {
+		boolean checkE = false;
+		for (int i = 0; i < memberArrayList.size();i++) {
+			if (email == memberArrayList.get(i).getEmail()) {
+				memberArrayList.remove(memberArrayList.get(i));
+				System.out.println("Email Deleted!");
+				checkE = true;
+			}
+		}
+		if (checkE == false) {
+			System.out.println("Email not found");
+		}
+	}
 	
+	//Sprint 2
+	public static void updateMember (String email) {
+		boolean checkEM = false;
+		for (int i = 0; i < memberArrayList.size();i++) {
+			if (email == memberArrayList.get(i).getEmail()) {
+				showUpdate();
+				int option = Helper.readInt("Enter Update Option > ");
+				if (option == 1) {
+					String pw = Helper.readString("Enter new password > ");
+					memberArrayList.get(i).setPw(pw);
+					System.out.println("Password Updated!");
+				} else if (option == 2) {
+					String cor = Helper.readString("Enter new country of residence > ");
+					memberArrayList.get(i).setCountry(cor);
+					System.out.println("Country of residence updated!");
+				} else if (option == 3) {
+					int no = Helper.readInt("Enter new mobile number > ");
+					memberArrayList.get(i).setNumber(no);
+					System.out.println("Mobile Updated!");
+				} else if (option == 4) {
+					String pw = Helper.readString("Enter new password > ");
+					String cor = Helper.readString("Enter new country of residence > ");
+					int no = Helper.readInt("Enter new mobile number > ");
+					memberArrayList.get(i).setPw(pw);
+					memberArrayList.get(i).setCountry(cor);
+					memberArrayList.get(i).setNumber(no);
+					System.out.println("Member Password, Country of residence and Mobile Updated!");
+				} else {
+					System.out.println("Invalid Option!");
+				}
+				checkEM = true;
+			}
+		}
+		if (checkEM == false) {
+			System.out.println("Invalid Option Entered.");
+		}
+		
+	}
+	
+	public static void searchMember(String country) {
+		boolean checkMem =false;
+		String output = (String.format("-20s %-20s %-20s -20s %-20s %-20s\n","Name", "Gender", "Mobile Number","Email","Date of Birth","Country of Residence"));
+		for(int i = 0; i < memberArrayList.size(); i++) {
+		if(country == memberArrayList.get(i).getCountry()) {
+			output += (String.format("-20s %-20s %-20d -20s %-20s %-20s\n", memberArrayList.get(i).getName(),memberArrayList.get(i).getGender() ,memberArrayList.get(i).getNumber(),memberArrayList.get(i).getEmail(), memberArrayList.get(i).getDOB(),memberArrayList.get(i).getCountry()));
+			checkMem =true;
+		}
+		}
+		if(checkMem =false) {
+			System.out.println("Invalid country");
+		} else {
+			System.out.println(output);
+		}
+	}
+	
+	public static void showMenu() {
+		Helper.line(40, "-");
+		System.out.println("1. Register Member");
+		System.out.println("2. View Member");
+		System.out.println("3. Delete Member");
+		//Sprint 2
+		System.out.println("4. Update Member Information");
+		System.out.println("5. Search Member");
+		System.out.println("6. Quit");
+	}
+	
+	//Sprint2
+	public static void showUpdate() {
+		Helper.line(40, "-");
+		System.out.println("1. Update Password");
+		System.out.println("2. Update Country of residence");
+		System.out.println("3. Update Mobile");
+		System.out.println("4. All of the above");
+	}
 
 }
