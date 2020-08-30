d326565e41fe87e72ced72d33466a64cc3b88dba
19016196Nisha <68525958+19016196Nisha@users.noreply.github.com>
2020-08-30 20:45:30 +0800

    Add files via upload

---
+++ C206_CaseStudy/src/courseDB.java
@@ -0,0 +1,12 @@
+package c206;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism
+ *
+ *19016196,30 Aug 2020,6:02:29 pm
+ */
+public class courseDB {
+
+}
