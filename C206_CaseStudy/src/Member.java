45bd4458a9b39759c49793c61e9935ad14d9d7da
18021723 <18021723@PFT-18021723.rp.edu.sg>
2020-08-30 12:38:25 +0800

    member1

--- C206_CaseStudy/src/Member.java
+++ C206_CaseStudy/src/Member.java
@@ -1,9 +1,3 @@
-* I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 18021723, 24 Aug 2020 1:11:02 PM
- */
 
 /**
  * I declare that this code was written by me.
---
+++ C206_CaseStudy/src/MemberMain.java
@@ -0,0 +1,27 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 18021723, 29 Aug 2020 11:42:14 PM
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 18021723, 29 Aug 2020 11:42:14 PM
+ */
+
+public class MemberMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
